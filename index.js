48af641810041ebb44bbbb52daebb58d84548e32
dncks3377 <70317911+dncks3377@users.noreply.github.com>
2020-10-03 13:23:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'NzU5MDAwODIyNTYxMzc0MjA4.X23ImQ.CAlZQtqXONeh6XQVjUhrSPYtIQA';
+const token = process.env.token;
 const welcomeChannelName = "안녕하세요";
 const byeChannelName = "안녕히가세요";
 const welcomeChannelComment = "어서오세요.";
